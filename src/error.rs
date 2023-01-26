d636fea358adde9af5dd2a178028629e081bda0a
littzhch <2371050115@qq.com>
2023-01-26 21:54:05 +0800

    finish v0.1.0

--- src/error.rs
+++ src/error.rs
@@ -1,45 +1,58 @@
+use reqwest::Error as RqError;
 use std::error::Error;
 use std::fmt::{Display, Formatter};
-use reqwest::Error as RqError;
 
+///
+///  The error kind used by `CasError`
+/// 
+/// Use `match` to process different kind.
+///
 #[derive(Copy, Clone, Debug)]
 pub enum ErrorKind {
     UserInfoIncorrect,
     ServiceUrlIncorrect,
     NetworkError,
-    Other,
 }
 
+///
+/// The error type.
+///
+/// There are only three kinds of errors,
+/// use `kind()` method to get `ErrorKind`.
+///
+/// Use `get_ref()`, `get_mut()`, `into_inner()`, or `source()` method to get
+/// the underlying error.
+///
 #[derive(Debug)]
 pub struct CasError {
     kind: ErrorKind,
-    inner: Box<dyn Error + Send + Sync + 'static>,
+    inner: Option<Box<dyn Error + Send + Sync + 'static>>,
 }
 
-
 impl CasError {
-    fn new<E>(kind: ErrorKind, inner: E) -> Self
-    where E: Error + Send + Sync + 'static
+    pub(crate) fn with_source<E>(kind: ErrorKind, inner: E) -> Self
+    where
+        E: Error + Send + Sync + 'static,
     {
-        Self {kind, inner: Box::new(inner)}
+        Self {
+            kind,
+            inner: Some(Box::new(inner)),
+        }
     }
 
-    fn other<E>(error: E) -> Self
-    where E: Error + Send + Sync + 'static
-    {
-        Self::new(ErrorKind::Other, error)
+    pub(crate) fn new(kind: ErrorKind) -> Self {
+        Self {kind, inner: None}
     }
 
-
-    pub fn get_ref(&self) -> &(dyn Error + Send + Sync + 'static) {
-        self.inner.as_ref()
+    pub fn get_ref(&self) -> Option<&(dyn Error + Send + Sync + 'static)> {
+        self.inner.as_ref().map(|e| e.as_ref())
     }
 
-    pub fn get_mut(&mut self) -> &mut (dyn Error + Send + Sync + 'static) {
-        self.inner.as_mut()
+    pub fn get_mut(&mut self) -> Option<&mut (dyn Error + Send + Sync + 'static)> {
+        self.inner.as_mut().map(|e| e.as_mut())
     }
 
-    pub fn into_inner(self) -> Box<dyn Error + Send + Sync + 'static> {
+    pub fn into_inner(self) -> Option<Box<dyn Error + Send + Sync + 'static>> {
         self.inner
     }
 
@@ -54,29 +67,25 @@ impl Display for CasError {
         match self.kind {
             UserInfoIncorrect => {
                 write!(f, "Username or password incorrect")
-            },
+            }
             ServiceUrlIncorrect => {
                 write!(f, "Service url incorrect")
-            },
+            }
             NetworkError => {
                 write!(f, "Network failed")
             },
-            Other => {
-                write!(f, "{}", self.get_ref())
-            }
         }
     }
 }
 
-
 impl Error for CasError {
     fn source(&self) -> Option<&(dyn Error + 'static)> {
-        Some(self.get_ref())
+        self.get_ref().map(|e| e as &(dyn Error + 'static))
     }
 }
 
 impl From<RqError> for CasError {
     fn from(value: RqError) -> Self {
-        Self::new(ErrorKind::NetworkError, value)
+        Self::with_source(ErrorKind::NetworkError, value)
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,47 @@
+//! a simple library for logging into USTC CAS System.
+//!
+//! # Usage
+//! All you should do is call `ustc_cas::get_ticket`. The function param `service_url` can
+//! be found from browser's address bar when logging into CAS by hand. The returned ticket value
+//! can be used for further authentication specific to websites.
+//!
+//! # example
+//! ```rust
+//! use std::error::Error;
+//! use tokio::runtime::Builder;
+//!
+//! fn main() -> Result<(), Box<dyn Error>> {
+//!     let runtime = Builder::new_current_thread()
+//!         .enable_io()
+//!         .enable_time()
+//!         .build()?;
+//!
+//!     let result = runtime.block_on(ustc_cas::get_ticket(
+//!         "PB00000000",
+//!         "12345678",
+//!         "https://jw.ustc.edu.cn/ucas-sso/login",
+//!     ));
+//!
+//!     match result {
+//!         Ok(s) => {
+//!             println!("ticket: {s}");
+//!         },
+//!         Err(e) => {
+//!             println!("Error: {e}");
+//!         }
+//!     }
+//!
+//!     Ok(())
+//! }
+//! ```
+//!
+//! # features
+//! - `native-tls`: Use system tls library. Enabled by default.
+//! - `rustls-tls`: Use rustls instead of system tls library. Disable `default` feature before
+//! enabling this feature.
+//!
+
+
 mod error;
 mod validate_code;
 
@@ -9,6 +53,14 @@ use regex::Regex;
 use reqwest::redirect::Policy;
 use reqwest::Client;
 
+///
+/// log into USTC CAS System and get ticket value.
+///
+/// # Panic
+///
+/// The function may panic if the CAS interface changed. Panic is considered
+/// a bug and needs to be fixed.
+///
 pub async fn get_ticket<U, P, S>(
     username: U,
     password: P,
@@ -49,10 +101,43 @@ where
         .unwrap();
 
     let text = rsps.text().await.unwrap();
+    let mut form = get_form(text)?;
+    form.insert("username".into(), username.into());
+    form.insert("password".into(), password.into());
+    if form["showCode"] == "1" {
+        let rsps = client.get(IMAGE_URL).send().await?.error_for_status().unwrap();
+        let code = validate_code::get_validatecode(rsps.bytes().await.unwrap());
+        form.insert("LT".into(), code);
+    }
+    form.insert("button".into(), "".into());
 
-
-
-    todo!()
+    let rsps = client.post(URL).form(&form).send().await?.error_for_status().unwrap();
+    let ticket = &ticket_re.captures_iter(
+        rsps.headers()
+            .get("location")
+            .ok_or(CasError::new(ErrorKind::UserInfoIncorrect))?
+            .to_str().unwrap()
+    )
+        .next()
+        .ok_or(CasError::new(ErrorKind::UserInfoIncorrect))?
+        [1];
+    Ok(ticket.into())
 }
 
 
+fn get_form(data: String) -> Result<HashMap<String, String>, CasError> {
+    static RE: OnceCell<Regex> = OnceCell::new();
+    let re = RE.get_or_init(
+        || {Regex::new(r#"<input type="hidden"[\s\S]*?name="(\S*?)" value="(\S*?)""#).unwrap()}
+    );
+
+    let mut map = HashMap::new();
+    for cap in re.captures_iter(&data) {
+        map.insert(cap[1].to_string(), cap[2].to_string());
+    }
+    if map.is_empty() {
+        Err(CasError::new(ErrorKind::ServiceUrlIncorrect))
+    } else {
+        Ok(map)
+    }
+}
